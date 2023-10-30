409df307ff4965471036a774588888d14f987c2f
wiktoria0211 <149382166+wiktoria0211@users.noreply.github.com>
2023-10-30 14:54:51 +0100

    fix for loop

---
+++ dane.py
@@ -0,0 +1,6 @@
+users_list:list = [
+    {"name":"Wiktoria", "nick":"szmagala","posts":1},
+    {"name":"Monika", "nick":"monia", "posts":2},
+    {"name":"Bartek", "nick":"baran", "posts":45},
+    {"name":"Kamil", "nick":"Kamcio", "posts":69},
+]
---
+++ main.py
@@ -0,0 +1,16 @@
+
+from dane import users_list
+for users in users_list:
+    print(f'Twój znajomy {users["nick"]} dodał {users["posts"]}!!')
+
+
+
+
+
+
+
+
+
+
+
+

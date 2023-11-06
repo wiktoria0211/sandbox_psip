e7204bbd86a0510bb094d348a9e65b4eceb21d9a
wiktoria0211 <149382166+wiktoria0211@users.noreply.github.com>
2023-11-06 12:57:53 +0100

    chore: add create functionalities
    dodano funkcjonalnosc

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 
 from dane import users_list
+
+def add_user_to(users_list:list) -> None:
+    """
+    add objcet to list
+    :param users_list: lista  - user list
+    :return: None
+    """
+
+    name = input('podaj imie ?')
+    posts = input('podaj liczbe postow ?')
+    users_list.append({'nick':name,'posts':posts})
+
+add_user_to(users_list)
+add_user_to(users_list)
+add_user_to(users_list)
+
+
 for users in users_list:
     print(f'Twój znajomy {users["nick"]} dodał {users["posts"]}!!')
 

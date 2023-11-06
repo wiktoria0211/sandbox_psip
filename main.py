c6564f77a49936ff96076a660cfd85f9ddc4d1f3
wiktoria0211 <149382166+wiktoria0211@users.noreply.github.com>
2023-11-06 14:49:18 +0100

    chore: add gui

--- main.py
+++ main.py
@@ -1,52 +1,35 @@
 
 from dane import users_list
+from utils.my_functions import add_user_to, remove_user_from
+def gui()->None:
+    while True:
+        print(f'MENU: \n'
+              F'0: Zakoncz program \n'
+              F'1: Wyswietl uzytkownikow \n'
+              F'2: dodaj uzytkownikow \n'
+              F'3: Usun uzytkownika \n'
+              F'4: Modyfikuj uzytkownika'
+              )
+        menu_option = input('Podaj funkcje do wywolania')
+        print(f' Wybrano funkcje {menu_option}')
 
-def add_user_to(users_list:list) -> None:
-    """
-    add objcet to list
-    :param users_list: lista  - user list
-    :return: None
-    """
-
-    name = input('podaj imie ?')
-    posts = input('podaj liczbe postow ?')
-    users_list.append({'nick':name,'posts':posts})
-
-def remove_user_from(users_list: list) -> None:
-    """
-    remove object from list
-    :param users_list: user list
-    :return: None
-    """
-    tmp_list = []
-    name = input('podaj imie uzytkownika do usuniecia: ')
-    for user in users_list:
-        if user["name"]== name:
-            print(f'znaleziono uzytkownika {user}')
-            tmp_list.append(user)
-    print('znaleziono uzytkownikow:')
-    print('0: Usun wszytskich znalezionych uzytkownikow')
-    for numerek, user_to_be_removed in enumerate(tmp_list):
-        print(f'{numerek+1}. {user_to_be_removed}')
-    numer = int(input(f'wybierz numer uzytkownika do usuniecia: '))
-    if numer == 0:
-        for user in users_list:
-            if user['name'] == name:
-                users_list.remove(user)
-    else:
-        users_list.remove(tmp_list[numer-1])
-
-    print(numer)
-    print(tmp_list[numer-1])
-    users_list.remove(tmp_list[numer-1])
-
-remove_user_from(users_list)
-
-#print(users_list)
-for users in users_list:
-    print(f'Twój znajomy {users["nick"]} dodał {users["posts"]}!!')
-
-
+        match menu_option:
+            case '0':
+                print('koncze prace')
+                break
+            case '1':
+                print('lista uzytkownikow: ')
+                show_users_from(users_list)
+            case '2':
+                print('dodaje uzytkownika: ')
+                add_user_to(users_list)
+            case '3':
+                print('usun uzytkownika: ')
+                remove_user_from(users_list)
+            case '4':
+                print('modyfikuj uzytkownika')
+                print('to bedzie zrobione') # TODO add this function to my_functions
+gui(users_list)
 
 
 
---
+++ utils/my_functions.py
@@ -0,0 +1,69 @@
+def add_user_to(users_list:list) -> None:
+    """
+    add objcet to list
+    :param users_list: lista  - user list
+    :return: None
+    """
+
+    name = input('podaj imie ?')
+    posts = input('podaj liczbe postow ?')
+    users_list.append({'nick':name,'posts':posts})
+
+def remove_user_from(users_list: list) -> None:
+    """
+    remove object from list
+    :param users_list: user list
+    :return: None
+    """
+    tmp_list = []
+    name = input('podaj imie uzytkownika do usuniecia: ')
+    for user in users_list:
+        if user["name"] == name:
+            print(f'znaleziono uzytkownika {user}')
+            tmp_list.append(user)
+    print('znaleziono uzytkownikow:')
+    print('0: Usun wszytskich znalezionych uzytkownikow')
+    for numerek, user_to_be_removed in enumerate(tmp_list):
+        print(f'{numerek + 1}. {user_to_be_removed}')
+    numer = int(input(f'wybierz numer uzytkownika do usuniecia: '))
+    if numer == 0:
+        for user in users_list:
+            if user['name'] == name:
+                users_list.remove(user)
+    else:
+        users_list.remove(tmp_list[numer - 1])
+
+def show_users_from(users_list:list)->None:
+    for user in users_list:
+        print(f'twoj znajomy {user["name"]} dodal {user["posts"]}')
+
+        def gui() -> None:
+            while True:
+                print(f'MENU: \n'
+                      F'0: Zakoncz program \n'
+                      F'1: Wyswietl uzytkownikow \n'
+                      F'2: dodaj uzytkownikow \n'
+                      F'3: Usun uzytkownika \n'
+                      F'4: Modyfikuj uzytkownika'
+                      )
+                menu_option = input('Podaj funkcje do wywolania')
+                print(f' Wybrano funkcje {menu_option}')
+
+                match menu_option:
+                    case '0':
+                        print('koncze prace')
+                        break
+                    case '1':
+                        print('lista uzytkownikow: ')
+                        show_users_from(users_list)
+                    case '2':
+                        print('dodaje uzytkownika: ')
+                        add_user_to(users_list)
+                    case '3':
+                        print('usun uzytkownika: ')
+                        remove_user_from(users_list)
+                    case '4':
+                        print('modyfikuj uzytkownika')
+                        print('to bedzie zrobione')  # TODO add this function to my_functions
+
+        gui()

54cb00ee47a5c1046a1b0e983a443ad140ce9ef0
Monikaa2611 <149382361+Monikaa2611@users.noreply.github.com>
2023-11-20 13:29:35 +0100

    feat: add modify function

---
+++ notatki.py
@@ -0,0 +1,16 @@
+from dane import users_list
+
+def update_user(users_list: list[dict, dict]) -> None:
+    nick_of_user = input('podaj nick użytkownika do modyfikacji')
+    print(nick_of_user)
+    for user in users_list:
+        if user['nick'] == nick_of_user:
+            print('Znaleziono!!!!')
+            user['name'] = input('podaj nowe imie: ')
+            user['nick'] = input('podaj nowa ksywke: ')
+            user['posts'] = int(input('podaj liczbę postów: '))
+        # new_nick = input('podaj nową ksywe')
+
+update_user(users_list)
+for user in users_list:
+    print(user)

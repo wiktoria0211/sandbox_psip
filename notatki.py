022007bbc2a3652a31882c00aa61be2ebcca8148
wiktoria0211 <149382166+wiktoria0211@users.noreply.github.com>
2023-11-27 13:55:09 +0100

    chore: generowanie mapy

--- notatki.py
+++ notatki.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 import re
+import  folium
 
 
 nazwy_miejscowosci = ['Gdańsk', 'Warszawa', 'Wrocław']
@@ -19,7 +20,21 @@ def get_coordinates_of(city:str)->list[float,float]:
     response_html_longitude = response_html.select('.longitude')[1].text
     response_html_longitude = float(response_html_longitude.replace(',', '.'))
     return [response_html_latitude, response_html_longitude]
+#for item in nazwy_miejscowosci:
+    #print(get_coordinates_of(item))
+
+#zwrocic mape z pineska odnszaca sie do nazwy uzytkownika podanego z klawiatury
+#zwrocic mape z wszytskimi uzytkownikami z danej listy(znajomymi)
+### rysowanie mapy
+city= get_coordinates_of(city='Zamość')
+map = folium.Map(
+    location=[52.3, 21.0], #gdzie mapa ma byc wycentrowana
+    tiles="OpenStreetMap",
+    zoom_start=7,
+    )
 for item in nazwy_miejscowosci:
-    print(get_coordinates_of(item))
-
-
+    folium.Marker(
+        location=get_coordinates_of(city=item),
+        popup='GEOINFORMATYKA RZĄDZI OU YEEEEAH!'
+    ).add_to(map)
+map.save('mapka.html')
